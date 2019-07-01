9a3f52fc449f27bf5d15929e5253966558e239ee
Eric DeRienzo <ericderienzo@hotmail.com>
2019-06-30 19:04:31 -0700

    Made navbar transparent before scrolling

--- _site/assets/js/navscrolled.js
+++ _site/assets/js/navscrolled.js
@@ -1,18 +1,6 @@
-/**
- * Listen to scroll to change header opacity class
- */
-function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
-
-    if($(window).scrollTop() > startY){
-        $('.navbar').addClass("scrolled");
-    }else{
-        $('.navbar').removeClass("scrolled");
-    }
-}
-
-if($('.navbar').length > 0){
-    $(window).on("scroll load resize", function(){
-        checkScroll();
-    });
-}
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
