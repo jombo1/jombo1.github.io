45221d653bd03608e2e3306638f6deea3c779c27
Eric DeRienzo <ericderienzo@hotmail.com>
2021-11-24 00:31:48 -0700

    Fixed navbar and aboutme styling issues with mobile screens

--- _site/assets/js/navscrolled.js
+++ _site/assets/js/navscrolled.js
@@ -2,5 +2,11 @@ $(function () {
   $(document).scroll(function () {
 	  var $nav = $(".fixed-top");
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+		
 	});
+});
+
+var $nav = $(".fixed-top");
+$('.navbar').on('show.bs.collapse', function () {
+	$nav.addClass('scrolled');
 });
